81a5dd6830aa0ec5900efb0263cd3520a8cc3247
gabrielSustacha <gabo.sustacha@gmail.com>
2024-10-24 14:17:09 -0400

    Coneccion a la base de datos

--- backend_hamburgueseria/src/app.module.ts
+++ backend_hamburgueseria/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.BD_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '*/**/entities/*.{ts|js}'],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend_hamburgueseria/src/main.ts
+++ backend_hamburgueseria/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT);
+  console.log(`App corriendo ${await app.getUrl()}/apidoc`);
 }
 bootstrap();
